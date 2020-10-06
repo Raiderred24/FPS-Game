474f9d6bf15af2bc2557b382af3add91be1d8bbc
Hayden <haydenhurst64@gmail.com>
2020-10-05 21:02:13 -0500

    1st setup complete

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // Increase score
+        _currentScore += scoreIncrease;
+        // Update score display to new score
+       _currentScoreTextView.text =
+            "Current Score: " + _currentScore.ToString();
+    }
+    public void ExitLevel()
+    {
+        // Compare scores
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            // Save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+}
