b440cec92193398bfb82dc54c0be4e24c787876a
Hayden <haydenhurst64@gmail.com>
2020-10-25 02:14:26 -0500

    3rd Setup Base Code Complete

---
+++ Assets/Scripts/EnemyShooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : MonoBehaviour
+{
+    // Enemy fires projectiles
+
+    int health = 100;
+    public void TakeDamage(int damageToTake)
+    {
+        health -= damageToTake;
+        Debug.Log(health + " health remaining");
+
+        // Death check
+
+    }
+}
--- Assets/Scripts/FireWeapon.cs
+++ Assets/Scripts/FireWeapon.cs
@@ -8,6 +8,11 @@ public class FireWeapon : MonoBehaviour
     [SerializeField] Camera cameraController;
     [SerializeField] Transform rayOrigin;
     [SerializeField] float shootDistance = 10f;
+    [SerializeField] GameObject visualFeedbackObject;
+    [SerializeField] int weaponDamage = 50;
+    [SerializeField] LayerMask hitLayers;
+
+    RaycastHit objectHit; // Stores info on Raycast hit
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
@@ -24,15 +29,27 @@ public class FireWeapon : MonoBehaviour
         // Cast a debug ray
         Debug.DrawRay(rayOrigin.position, rayDirection * shootDistance, Color.yellow, 1f);
         // Do the raycast
-        if(Physics.Raycast(rayOrigin.position, rayDirection, shootDistance))
+        if(Physics.Raycast(rayOrigin.position, rayDirection, out objectHit, shootDistance, hitLayers))
         {
-            //Code hits
-            Debug.Log("Hit!");
+            // Code hit object /Graffitti tag?
+            Debug.Log("You hit the " + objectHit.transform.gameObject.name);
+            if (objectHit.transform.tag == "Enemy")
+            {
+                Debug.Log("Deal Damage");
+                // Visual feedback
+                visualFeedbackObject.transform.position = objectHit.point;
+                // Apply damage if target is an enemy
+                EnemyShooter enemyShooter = objectHit.transform.gameObject.GetComponent<EnemyShooter>();
+                if (enemyShooter != null)
+                {
+                    enemyShooter.TakeDamage(weaponDamage);
+                }
+            }
         }
         else 
         {
-            //Code misses
-            Debug.Log("Miss");   
+            // Code misses
+            Debug.Log("You Miss");   
         }
     }
 }
